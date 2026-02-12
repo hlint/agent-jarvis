4dad2a072bd90e10769ed7d8c5eb1a39a08eb3ee
hlint <hanlintang813@gmail.com>
2026-02-12 04:39:40 -0800

    Add onDialogHistoryChange call in processToolCalling function to update dialog history during tool execution.

--- packages/shared/agent/lib/process-tool.ts
+++ packages/shared/agent/lib/process-tool.ts
@@ -24,6 +24,7 @@ export default async function processToolCalling({
       toolInput: toolCall.input,
       toolOutput: null,
     });
+    onDialogHistoryChange();
 
     tasks.push(
       (async () => {
