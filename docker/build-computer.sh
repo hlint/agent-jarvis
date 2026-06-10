07114f69ecfe926aa279c77d8f2e9989447c6e2f
hlint <hanlintang813@gmail.com>
2026-06-09 18:28:30 -0700

    Update Docker image tags for agent-jarvis-computer to version 1.1.0 in build script and Dockerfile for consistency across environments.

--- docker/build-computer.sh
+++ docker/build-computer.sh
@@ -1,4 +1,4 @@
 
 docker buildx build --platform=linux/amd64,linux/arm64 --push \
-  -t hlint/agent-jarvis-computer:1.1.0-pre \
+  -t hlint/agent-jarvis-computer:1.1.0 \
 	-f ./computer/Dockerfile ./computer/assets
